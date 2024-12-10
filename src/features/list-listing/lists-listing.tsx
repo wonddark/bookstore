7abc8938d3e33e1ab2a639462f4409a2e7de5713
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-11 02:54:57 +0400

    Added horizontal padding to list listing page

--- src/features/list-listing/lists-listing.tsx
+++ src/features/list-listing/lists-listing.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function ListsListing() {
   return (
-    <div>
+    <div className="px-3 lg:px-5">
       <Link to="/lists/0">Default</Link>
     </div>
   );
