80c789ff7d69491c5c56e2301f63c303260222c7
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-11 02:52:27 +0400

    Keep the search term in the search input over navigation

--- src/features/search-form/search-form.tsx
+++ src/features/search-form/search-form.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input.tsx";
+import { useQueryState } from "nuqs";
 
 export default function SearchForm() {
+  const [query] = useQueryState("q", { defaultValue: "" });
   return (
     <form
       method="GET"
@@ -11,9 +13,10 @@ export default function SearchForm() {
       <input type="text" readOnly hidden name="p" value="1" />
       <Input
         type="search"
-        placeholder="Search"
+        placeholder="Search tech books"
         name="q"
         className="rounded-r-none"
+        defaultValue={query}
       />
       <Button className="rounded-l-none">
         <i className="fa-solid fa-search"></i>
