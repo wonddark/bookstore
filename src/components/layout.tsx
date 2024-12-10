8a99a32fae5b5c0e87c055f135727ee87d50eed8
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-11 02:56:40 +0400

    Improved the layout display and also rendered the ScrollToTop component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,19 +3,21 @@ import { Outlet } from "react-router-dom";
 import SearchForm from "@/features/search-form/search-form.tsx";
 import AppSidebar from "@/components/app-sidebar.tsx";
 import { Toaster } from "sonner";
+import ScrollToTop from "@/components/scroll-to-top.tsx";
 
 export default function Layout() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <main className="w-full">
-        <div className="flex py-3 border-b-4">
+        <div className="flex items-center gap-3 py-3 border-b-4 px-3 lg:px-5 sticky top-0 bg-white z-50">
           <SidebarTrigger />
           <SearchForm />
         </div>
         <Outlet />
       </main>
       <Toaster />
+      <ScrollToTop />
     </SidebarProvider>
   );
 }
