19603e4a9320a033b3ce54bf619d4bf7d7cdbf4e
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-11 02:57:41 +0400

    Added horizontal padding to book list page

--- src/features/books-listing/index.tsx
+++ src/features/books-listing/index.tsx
@@ -5,7 +5,7 @@ import SkeletonBookCard from "@/components/skeletons/skeleton-book-card.tsx";
 export default function BooksListing() {
   const { data, isLoading } = useRecentBooksQuery(undefined);
   return (
-    <div className="flex flex-col lg:flex-row flex-wrap gap-3 lg:gap-4 items-center lg:items-stretch justify-start lg:justify-center py-3 lg:py-4">
+    <div className="flex flex-col lg:flex-row flex-wrap gap-3 lg:gap-4 items-center lg:items-stretch justify-start lg:justify-center py-3 lg:py-4 px-3 lg:px-5">
       {isLoading &&
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
           <SkeletonBookCard key={item} />
